9f3ca57db5f46fdc1a0a284a21baaa948ab85b19
Dennis Palucki <34459336+denster32@users.noreply.github.com>
2025-07-06 13:35:39 -0500

    Initial Commit

---
+++ DogTV+Tests/DogTV_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DogTV_Tests.swift
+//  DogTV+Tests
+//
+//  Created by Denster on 7/6/25.
+//
+
+import Testing
+@testable import DogTV_
+
+struct DogTV_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ DogTV+UITests/DogTV_UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DogTV_UITestsLaunchTests.swift
+//  DogTV+UITests
+//
+//  Created by Denster on 7/6/25.
+//
+
+import XCTest
+
+final class DogTV_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
